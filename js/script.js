f7988d5c9d18c79caa2e406f42223647a592086c
KoKaLiNe <94233674+KoKaLiNe@users.noreply.github.com>
2022-03-07 18:47:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,29 @@
   
     window.addEventListener('scroll', trackScroll);
   })();
+
+
+
+
+  // Задание 2 Вывод информации о дате
+
+function getDayInfo(date) {
+
+    dateParts = date.split('.');
+    date = new Date(dateParts[1] + '-' + dateParts[0] + '-' + dateParts[2]);
+
+    weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Восскресенье'];
+    monthNames = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+
+    weekDay = weekDays[date.getDay()-1];
+    weekNumber = Math.ceil(dateParts[0] / 7);
+    month = monthNames[date.getMonth()];
+    year = dateParts[2];
+
+    console.log(weekDay + ', ' + weekNumber + ' неделя ' + month + ' ' + year + ' года');
+
+}
+
+
+getDayInfo('01.01.2022');
+getDayInfo('15.12.2021');
